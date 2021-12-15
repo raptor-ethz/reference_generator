421a4f1faba93c93a348726c6c58f8d9e55869a7
Pascal Strauch <pstrauch@ethz.ch>
2021-12-15 17:18:58 +0100

    Implement land_in_stand

--- apps/land_in_stand/src/main.cpp
+++ apps/land_in_stand/src/main.cpp
@@ -1,5 +1,22 @@
-#include <iostream>
+#include "Quad.h"
+#include "Item.h"
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+
+int main() {
+    // FastDDS default participant
+    std::unique_ptr<DefaultParticipant> dp =
+        std::make_unique<DefaultParticipant>(0, "raptor");
+
+    // create instance of quad
+    Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
+
+    // creat instance of stand
+    Item stand("Stand", dp, "mocap_srl_stand");
+
+    quad.go_to_pos(-1.5, 1, 2, 2000, false);
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    quad.land(stand);
+
 }

0df9106121bdaf7e1975843a60e57927bab8c647
aurelappius <appiusa@student.ethz.ch>
2021-12-15 19:09:53 +0100

    corrected land_in_stand yaw

--- apps/land_in_stand/src/main.cpp
+++ apps/land_in_stand/src/main.cpp
@@ -1,22 +1,20 @@
-#include "Quad.h"
 #include "Item.h"
-
+#include "Quad.h"
 
 int main() {
-    // FastDDS default participant
-    std::unique_ptr<DefaultParticipant> dp =
-        std::make_unique<DefaultParticipant>(0, "raptor");
+  // FastDDS default participant
+  std::unique_ptr<DefaultParticipant> dp =
+      std::make_unique<DefaultParticipant>(0, "raptor");
 
-    // create instance of quad
-    Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
+  // create instance of quad
+  Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
 
-    // creat instance of stand
-    Item stand("Stand", dp, "mocap_srl_stand");
+  // creat instance of stand
+  Item stand("Stand", dp, "mocap_srl_stand");
 
-    quad.go_to_pos(-1.5, 1, 2, 2000, false);
+  quad.go_to_pos(-1.5, 1, 2, 0, 2000, false);
 
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+  std::this_thread::sleep_for(std::chrono::seconds(2));
 
-    quad.land(stand);
-
+  quad.land(stand);
 }

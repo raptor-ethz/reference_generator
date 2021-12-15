80e8f68895032d4e62e69a1299b7811e5bb5beed
aurelappius <appiusa@student.ethz.ch>
2021-12-15 20:36:02 +0100

    tuned parameters

--- apps/parabola/main.cpp
+++ apps/parabola/main.cpp
@@ -1,24 +1,18 @@
 #include "Quad.h"
 
-
 void discretize_parabola(std::vector<std::vector<double>> &discretization,
-                        const int gridPoints,
-                        const double a,
-                        const double b,
-                        const double c)
-{
+                         const int gridPoints, const double a, const double b,
+                         const double c) {
   // z = a * x * x + b * x + c
 
-  double step = 4/double(gridPoints);
+  double step = 4 / double(gridPoints);
 
-  for(double t = -2.0; t <= 2.0; t += step)
-  {
-      discretization.at(0).push_back(t);
-      discretization.at(1).push_back(a*t*t + b*t + c);
+  for (double t = -2.0; t <= 2.0; t += step) {
+    discretization.at(0).push_back(t);
+    discretization.at(1).push_back(a * t * t + b * t + c);
   }
-  
-  assert(discretization.at(0).size() >= gridPoints);
 
+  assert(discretization.at(0).size() >= gridPoints);
 }
 
 int main() {
@@ -26,43 +20,39 @@ int main() {
   std::unique_ptr<DefaultParticipant> dp =
       std::make_unique<DefaultParticipant>(0, "raptor");
 
-    // create instance of quad
-    Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
+  // create instance of quad
+  Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
 
-    // creat instance of landing stand
-    Item stand("Stand", dp, "mocap_srl_stand");
+  // creat instance of landing stand
+  Item stand("Stand", dp, "mocap_srl_stand");
 
-    quad.check_for_data();
-    stand.check_for_data();
+  quad.check_for_data();
+  stand.check_for_data();
 
+  const int gridPoints = 80;  // -> 6second per circle
 
-    const int gridPoints = 300; // -> 6second per circle
+  const double a = 0.375;
+  const double b = 0.0;
+  const double c = 0.5;
 
-    const double a = 0.375;
-    const double b = 0.0;
-    const double c = 0.5;
+  std::vector<std::vector<double>> parabola(2);
 
-    std::vector<std::vector<double>> parabola(2);
+  discretize_parabola(parabola, gridPoints, a, b, c);
 
-    discretize_parabola(parabola, gridPoints, a, b, c);
+  quad.set_thresh(0.25, 0.25, 0.25);
 
-    quad.set_thresh(0.05, 0.05, 0.05);
+  // go to inital position
+  quad.go_to_pos(-2.0, 1, 2, 0, 5000, false);
 
-    // go to inital position
-    quad.go_to_pos(-2.0, 1, 2, 0, 5000, false);
+  // control loop
+  for (int i = 0; i < gridPoints; ++i) {
+    quad.go_to_pos(parabola.at(0).at(i), 1, parabola.at(1).at(i), 0, 50, true);
+    // std::cout << "Reference: " << parabola.at(0).at(i) << "\t"
+    //           << parabola.at(1).at(i) << std::endl;
+  }
 
-    // control loop
-    for (int i = 0; i < gridPoints; ++i)
-    {
-      quad.go_to_pos(parabola.at(0).at(i),
-                      1, parabola.at(1).at(i), 0, 20, true);
-      // std::cout << "Reference: " << parabola.at(0).at(i) << "\t" 
-      //           << parabola.at(1).at(i) << std::endl;
-    }
-    
+  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
 
-  
-
-    // go back to initial position
-    quad.land(stand);
+  // go back to initial position
+  quad.land(stand);
 }

52c16eeb70f6e7000af3793a355b42ad9952a45f
aurelappius <appiusa@student.ethz.ch>
2021-12-17 14:07:03 +0100

    grab 2 objects adn dropoff point

--- apps/grab_object/src/main.cpp
+++ apps/grab_object/src/main.cpp
@@ -13,23 +13,34 @@ int main() {
   Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
   Item stand("Stand", dp, "mocap_srl_stand");
   Item box("Box", dp, "mocap_srl_box");
+  Item drop("drop", dp, "mocap_srl_drop");
   Gripper gripper("Gripper", dp, "grip_cmd");
 
   // check for data
   quad.check_for_data();
   stand.check_for_data();
   box.check_for_data();
+  drop.check_for_data();
 
   // open gripper
   gripper.set_angle(45);
+  // first swoop
+  quad.swoop(box, gripper, 2, 0, 0.05, 2, 2000);
 
-  quad.swoop(box, gripper, 2, 2, 2000);
+  sleep_for(milliseconds(1000));
 
-  sleep_for(milliseconds(750));
+  quad.release(drop, gripper, 1.5, 2, 2000);
 
-  gripper.set_angle(45);
+  sleep_for(milliseconds(1000));
 
-  sleep_for(milliseconds(300));
+  // second swoop
+  quad.swoop(box, gripper, 2, 0, -0.25, 2, 2000);
+
+  sleep_for(milliseconds(1000));
+
+  quad.release(drop, gripper, 1.5, 2, 2000);
+
+  sleep_for(milliseconds(1000));
 
   gripper.set_angle(0);
 

ea44aaec5e5365c0d9dd001a4d09d2114f3f3867
aurelappius <appiusa@student.ethz.ch>
2021-12-22 14:33:58 +0100

    gripper test individual

--- apps/gripper_test/src/main.cpp
+++ apps/gripper_test/src/main.cpp
@@ -11,17 +11,17 @@ int main() {
     gripper.set_front_arm(79);
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    // std::cout << "back arm to 45" << std::endl;
-    // gripper.set_back_arm(45);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    std::cout << "back arm to 45" << std::endl;
+    gripper.set_back_arm(45);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
     std::cout << "front arm to 0" << std::endl;
     gripper.set_front_arm(0);
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    // std::cout << "back arm to 0" << std::endl;
-    // gripper.set_back_arm(0);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    std::cout << "back arm to 0" << std::endl;
+    gripper.set_back_arm(0);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
   return 0;
 }

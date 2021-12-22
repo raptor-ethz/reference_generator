71231e2be195673eb65c94f5e254915691c104e4
aurelappius <appiusa@student.ethz.ch>
2021-12-22 14:23:14 +0100

    gripper max angle test

--- apps/gripper_test/src/main.cpp
+++ apps/gripper_test/src/main.cpp
@@ -8,20 +8,20 @@ int main() {
   Gripper gripper("Gripper", dp, "grip_cmd");
   while (true) {
     std::cout << "front arm to 45" << std::endl;
-    gripper.set_front_arm(45);
+    gripper.set_front_arm(79);
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    std::cout << "back arm to 45" << std::endl;
-    gripper.set_back_arm(45);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    // std::cout << "back arm to 45" << std::endl;
+    // gripper.set_back_arm(45);
+    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
     std::cout << "front arm to 0" << std::endl;
     gripper.set_front_arm(0);
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    std::cout << "back arm to 0" << std::endl;
-    gripper.set_back_arm(0);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    // std::cout << "back arm to 0" << std::endl;
+    // gripper.set_back_arm(0);
+    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
   return 0;
 }

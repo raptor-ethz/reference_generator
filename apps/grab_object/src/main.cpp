9ea9cacfc39c48f924e74064531bcd486b635341
aurelappius <appiusa@student.ethz.ch>
2022-02-23 21:00:32 +0100

    periodic swooping

--- apps/grab_object/src/main.cpp
+++ apps/grab_object/src/main.cpp
@@ -13,28 +13,38 @@ int main() {
   Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
   Item stand("Stand", dp, "mocap_srl_stand");
   Item box("Box", dp, "mocap_srl_box");
-  Item drop("drop", dp, "mocap_srl_drop");
+  //Item drop("drop", dp, "mocap_srl_drop");
   Gripper gripper("Gripper", dp, "grip_cmd");
 
-  // check for data
+  // check for data (MOCAP)
   quad.check_for_data();
   stand.check_for_data();
   box.check_for_data();
-  drop.check_for_data();
+  //drop.check_for_data();
 
   // first swoop
-  quad.quick_swoop(box, gripper, 2, 0.05, 0.03, 0, 2, 100, 3);
-  quad.quick_release(drop, gripper, 1.5, 2, 2000);
+  //quad.swoop(box,gripper,2,0,0,0,2,0,45);
+  for(int i=0; i<5; i++){
+    std::cout<<"FLIGHT_NR"<<i<<std::endl;
+    quad.quick_swoop(box, gripper, 2, 0.05, 0.03, 0, 2, 50, 3);
 
-  // second swoop
-  quad.quick_swoop(box, gripper, 2, 0.05, 0.33, -0.04, 2, 200, 0);
-  quad.quick_release(drop, gripper, 1.5, 2, 2000);
+    gripper.set_angle_sym(60);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    gripper.set_angle_sym(5);
 
-  // third swoop
-  quad.quick_swoop(box, gripper, 2, 0.05, -0.26, -0.01, 2, 100, 0);
-  quad.quick_release(drop, gripper, 1.5, 2, 2000);
+    quad.go_to_pos(-2, 0.5, 2, 0, 4000, false);
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+  }
 
-  gripper.set_angle_sym(0);
+  // quad.quick_release(drop, gripper, 1.5, 2, 2000);
+
+  // // second swoop
+  // quad.quick_swoop(box, gripper, 2, 0.05, 0.33, -0.04, 2, 200, 0);
+  // quad.quick_release(drop, gripper, 1.5, 2, 2000);
+
+  // // third swoop
+  // quad.quick_swoop(box, gripper, 2, 0.05, -0.26, -0.01, 2, 100, 0);
+  // quad.quick_release(drop, gripper, 1.5, 2, 2000);
 
   quad.land(stand);
 

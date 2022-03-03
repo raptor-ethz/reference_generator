6da5cb568ea4bf537c29aefc2c7bd236ee8bcfa5
Pascal Strauch <pstrauch@ethz.ch>
2022-03-03 16:09:28 +0100

    Add px4_cmd_test application

---
+++ apps/px4_cmd_test/src/main.cpp
@@ -0,0 +1,27 @@
+#include "Gripper.h"
+#include "MocapPubSubTypes.h"
+int main() {
+  // FastDDS default participant
+  std::unique_ptr<DefaultParticipant> dp =
+      std::make_unique<DefaultParticipant>(0, "raptor");
+
+  Gripper gripper("Gripper", dp, "grip_cmd");
+  while (true) {
+    std::cout << "front arm to 45" << std::endl;
+    gripper.set_front_arm(79);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    std::cout << "back arm to 45" << std::endl;
+    gripper.set_back_arm(79);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    std::cout << "front arm to 5" << std::endl;
+    gripper.set_front_arm(5);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+    std::cout << "back arm to 5" << std::endl;
+    gripper.set_back_arm(5);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+  return 0;
+}

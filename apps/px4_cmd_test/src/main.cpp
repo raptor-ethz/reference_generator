3856255baac741e69271a049f472d68bcba9ac98
Pascal Strauch <pstrauch@ethz.ch>
2022-03-09 18:52:54 +0100

    Add threads

--- apps/px4_cmd_test/src/main.cpp
+++ apps/px4_cmd_test/src/main.cpp
@@ -1,34 +1,70 @@
-#include "Quad.h"
-// #include "MocapPubSubTypes.h"
 #include "HeaderPubSubTypes.h"
+#include "Quad.h"
 #include "QuadPositionCmdPubSubTypes.h"
-int main() {
-  // FastDDS default participant
-  std::unique_ptr<DefaultParticipant> dp =
-      std::make_unique<DefaultParticipant>(0, "raptor");
-      
-    // gripper.set_back_arm(5);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-  Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
-  Item stand("Stand", dp, "mocap_srl_stand");
+// FastDDS default participant
+std::unique_ptr<DefaultParticipant> dp =
+    std::make_unique<DefaultParticipant>(0, "raptor");
+
+Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
+Item stand("Stand", dp, "mocap_srl_stand");
+
+bool terminate_mission = false;
+bool mission_flag = true;
+
+void flyMission()
+{
+  quad.takeOff();
+
+  if (terminate_mission) {
+    return;
+  }
+
+  quad.goToPos(1, 1, 2, 0, 4000, false);
+  if (terminate_mission) {
+    return;
+  }
+  quad.goToPos(0, 0, 2, 0, 4000, false);
+  if (terminate_mission) {
+    return;
+  }
+
+  quad.land(stand);
+
+  mission_flag = false;
+}
+
+int main()
+{
   // check for data
   quad.checkForData();
   stand.checkForData();
 
-  /* SETUP & TAKEOFF */
   // temporary
   quad.setState(initialized);
 
-  quad.takeOff();
-  /* SETUP & TAKEOFF */
+  std::thread mission_thread(flyMission);
 
-  /* MISSION */
-  quad.goToPos(1, 1, 2, 0, 4000, false);
-  quad.goToPos(0, 0, 2, 0, 4000, false);
-  /* END MISSION */
+  while (mission_flag) {
+    std::cout << "Enter 'l' to land next: " << std::endl;
+    char input;
+    std::cin >> input;
+    switch (input) {
+    case 'l':
+      terminate_mission = true;
+      mission_thread.join();
+      quad.land(stand);
+      mission_flag = false;
+      break;
 
-  quad.land(stand);
+    default:
+      std::cout << "Unrecognized command: " << input << std::endl;
+      break;
+    }
+  }
+
+  // wait for the mission to finish
+  mission_thread.join();
 
   return 0;
 }

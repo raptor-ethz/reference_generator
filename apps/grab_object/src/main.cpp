91a56fa85f0071380edfb99063061f4be865629e
Pascal Strauch <pstrauch@ethz.ch>
2022-03-09 22:05:37 +0100

    Handle feedback from checks and actions

--- apps/grab_object/src/main.cpp
+++ apps/grab_object/src/main.cpp
@@ -14,44 +14,41 @@ int main() {
   Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
   Item stand("Stand", dp, "mocap_srl_stand");
   Item box("Box", dp, "mocap_srl_box");
-  //Item drop("drop", dp, "mocap_srl_drop");
+  // Item drop("drop", dp, "mocap_srl_drop");
   Gripper gripper("Gripper", dp, "grip_cmd");
   /* END CREATE PARTICIPANTS */
 
-  /* CHECK MOCAP DATA */
-  quad.checkForData();
-  stand.checkForData();
-  box.checkForData();
-  //drop.checkForData();
-
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  /* END CHECK MOCAP DATA */
-
-  /* SETUP & TAKEOFF */
-  // temporary
+  // check mocap data
+  if (!quad.checkForData() || !stand.checkForData() || !box.checkForData()) {
+    std::cerr << "Fatal Error (main:23) Terminate Process." << std::endl;
+    return 1;
+  }
   quad.setState(initialized);
-  quad.takeOff();
-  /* SETUP & TAKEOFF */
+
+  if (!quad.takeOff()) {
+    // TODO: try again after 10s
+    std::cerr << "Fatal Error (main:29) Terminate Process." << std::endl;
+    return 1;
+  }
 
   // first swoop
-  //quad.swoop(box,gripper,2,0,0,0,2,0,45);
-  //for(int i=0; i<1; i++){
-    
-    quad.quickSwoop(box, gripper, 2, 0.05, 0.03, -0.02, 2, 50, 3); //potato
-    //quad.quick_swoop(box, gripper, 2, 0.05, 0.03, -0.035, 2, 50, 0); //box
-    // quad.quick_swoop(box, gripper, 2, 0.05, 0.05, 0, 2, 50, 0); //paper_roll
+  // quad.swoop(box,gripper,2,0,0,0,2,0,45);
+  // for(int i=0; i<1; i++){
 
-    gripper.set_angle_sym(60);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    gripper.set_angle_sym(5);
-    
-    
-    //FOTOS
-    // std::this_thread::sleep_for(std::chrono::milliseconds(6000));
-    // gripper.set_angle_sym(60);
-    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    // gripper.set_angle_sym(5);
-    
+  quad.quickSwoop(box, gripper, 2, 0.05, 0.03, -0.02, 2, 50, 3); // potato
+  // quad.quick_swoop(box, gripper, 2, 0.05, 0.03, -0.035, 2, 50, 0); //box
+  // quad.quick_swoop(box, gripper, 2, 0.05, 0.05, 0, 2, 50, 0); //paper_roll
+
+  gripper.set_angle_sym(60);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  gripper.set_angle_sym(5);
+
+  // FOTOS
+  // std::this_thread::sleep_for(std::chrono::milliseconds(6000));
+  // gripper.set_angle_sym(60);
+  // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  // gripper.set_angle_sym(5);
+
   //   quad.go_to_pos(-2, 0.5, 2, 0, 4000, false);
   //   std::this_thread::sleep_for(std::chrono::milliseconds(2000));
   // }

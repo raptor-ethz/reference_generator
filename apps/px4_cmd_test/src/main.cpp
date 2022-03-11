3b888c06e87d955641997bbc5fbb4ee4fb45e1f7
Pascal Strauch <pstrauch@ethz.ch>
2022-03-11 12:16:08 +0100

    Log config

--- apps/px4_cmd_test/src/main.cpp
+++ apps/px4_cmd_test/src/main.cpp
@@ -27,6 +27,8 @@ int main() {
   std::thread th_log(startLog, std::ref(log_flag), "mocap_srl_quad");
   th_log.detach();
 
+  // wait for logger to initialize
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   
   quad.goToPos(0, 1, 2, 0, 5000, true);
   quad.goToPos(2, 1, 2, 0, 5000, true);

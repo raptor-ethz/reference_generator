185e7d28cd7456d0438f02cf61959ba924363d3e
Pascal Strauch <pstrauch@ethz.ch>
2022-03-15 16:28:00 +0100

    Change mocap fail test to stand only

--- apps/mocap_fail_test/src/main.cpp
+++ apps/mocap_fail_test/src/main.cpp
@@ -9,23 +9,25 @@ int main() {
   std::unique_ptr<DefaultParticipant> dp =
       std::make_unique<DefaultParticipant>(0, "raptor");
 
-  Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
+  // Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
+
   Item stand("Stand", dp, "mocap_srl_stand");
 
   // check for data
-  quad.checkForData();
+  // quad.checkForData();
   stand.checkForData();
 
   // temporary
-  quad.setState(initialized);
+  // quad.setState(initialized);
 
   // start logger
   std::atomic<LogFlag> log_flag{run};
-  std::thread th_log(startLog, std::ref(log_flag), "mocap_srl_quad");
+  std::thread th_log(startLog, std::ref(log_flag), "mocap_srl_stand");
 
   // wait
   for (int i = 0; i < 10; ++i) {
     std::cout << 10-i << std::endl;
+    std::cout << stand.getPose().header.timestamp << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
 

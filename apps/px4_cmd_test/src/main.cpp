e5cf2113919b2de9189871b9239a1a5d98e0bca2
Pascal Strauch <pstrauch@ethz.ch>
2022-03-23 10:40:50 +0100

    Feedback test

--- apps/px4_cmd_test/src/main.cpp
+++ apps/px4_cmd_test/src/main.cpp
@@ -2,6 +2,7 @@
 #include "Quad.h"
 #include "QuadPositionCmdPubSubTypes.h"
 #include "logger.h"
+#include "std_msgs/msgs/Header.h"
 #include <thread>
 
 int main() {
@@ -9,36 +10,50 @@ int main() {
   std::unique_ptr<DefaultParticipant> dp =
       std::make_unique<DefaultParticipant>(0, "raptor");
 
-  Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
-  Item stand("Stand", dp, "mocap_srl_stand");
+  cpp_msg::Header px4_info;
 
-  // check for data
-  quad.checkForData();
-  stand.checkForData();
+  DDSSubscriber px4_info_sub_(idl_msg::HeaderPubSubType(), &px4_info,
+                              "px4_status_msgs", dp->participant());
+
+  DDSPublisher px4_action_pub_(idl_msg::HeaderPubSubType(), "px4_commands",
+                               dp->participant());
+
+  cpp_msg::Header px4_action_cmd_{};
+  px4_action_cmd_.id = "info";
+  px4_action_pub_.publish(px4_action_cmd_);
+
+  for (int i = 0; i < 100; ++i) {
+    px4_action_cmd_.id = "info";
+    px4_action_pub_.publish(px4_action_cmd_);
+    std::cout << "ID: " << px4_info.id << std::endl;
+    std::cout << "STMP: " << (int)px4_info.timestamp << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  // Quad quad("Quad", dp, "mocap_srl_quad", "pos_cmd");
+  // Item stand("Stand", dp, "mocap_srl_stand");
+
+  // // check for data
+  // quad.checkForData();
+  // stand.checkForData();
 
   // temporary
-  quad.setState(initialized);
+  // quad.setState(initialized);
 
-  quad.takeOff();
-  quad.goToPos(0, -1, 2, 0, 5000, false);
+  // quad.takeOff();
 
   // start logger
-  std::atomic<LogFlag> log_flag{run};
-  std::thread th_log(startLog, std::ref(log_flag), "mocap_srl_quad");
-  th_log.detach();
+  // std::atomic<LogFlag> log_flag{run};
+  // std::thread th_log(startLog, std::ref(log_flag), "mocap_srl_quad");
+  // th_log.detach();
 
   // wait for logger to initialize
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  
-  quad.goToPos(0, 1, 2, 0, 5000, true);
-  quad.goToPos(2, 1, 2, 0, 5000, true);
-  quad.goToPos(2, -1, 2, 0, 5000, true);
-  quad.goToPos(0, -1, 2, 0, 5000, true);
+  // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-  //stop logger
-  log_flag.store(stop);
-  
-  quad.land(stand);
+  // stop logger
+  // log_flag.store(stop);
+
+  // quad.land(stand);
 
   return 0;
 }

e1b21c5e11c3197f9dff35ca8000300534a587a0
aurel <appiusa@student.ethz.ch>
2022-03-24 12:59:51 +0100

    added section for HASEL gripper

--- apps/gripper_test/src/main.cpp
+++ apps/gripper_test/src/main.cpp
@@ -6,22 +6,34 @@ int main() {
       std::make_unique<DefaultParticipant>(0, "raptor");
 
   Gripper gripper("Gripper", dp, "grip_cmd");
-  while (true) {
-    std::cout << "front arm to 45" << std::endl;
-    gripper.set_front_arm(79);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    std::cout << "back arm to 45" << std::endl;
-    gripper.set_back_arm(79);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  /* rotational gripper */
+  // while (true) {
+  //   std::cout << "front arm to 45" << std::endl;
+  //   gripper.set_front_arm(79);
+  //   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    std::cout << "front arm to 5" << std::endl;
-    gripper.set_front_arm(5);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  //   std::cout << "back arm to 45" << std::endl;
+  //   gripper.set_back_arm(79);
+  //   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
-    std::cout << "back arm to 5" << std::endl;
-    gripper.set_back_arm(5);
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-  }
+  //   std::cout << "front arm to 5" << std::endl;
+  //   gripper.set_front_arm(5);
+  //   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+  //   std::cout << "back arm to 5" << std::endl;
+  //   gripper.set_back_arm(5);
+  //   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  // }
+
+  /* HASEL gripper */
+  std::cout << "closing HASEL gripper" << std::endl;
+  gripper.set_front_arm(0);
+  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+  std::cout << "opening HASEL gripper" << std::endl;
+  gripper.set_front_arm(1);
+  std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
   return 0;
 }
